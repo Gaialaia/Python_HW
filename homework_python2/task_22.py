af360ac2347f1c3e66fc980e943588eb3e1f9cfa
[Gaia <[dzhayadzoy@gmail.com]>
2023-08-31 13:47:16 +0500

    task_22_solution

--- homework_python2/task_22.py
+++ homework_python2/task_22.py
@@ -1,54 +1,11 @@
 #task 22
 
-#
-# s = set()
-#
-# s.add(input())
-#
-# print(s)
-# print(type(s))
-#
-# # for i in s:
-# #     s.add(input())
-# #     len(s)
+#task 22
 
+setonelength = int(input('enter setone length   '))
+setone = {input(i ) for i in range(setonelength)}
+settwolength = int(input('enter settwo length   '))
+settwo = {input(i ) for i in range(settwolength)}
+setthree = sorted(setone.intersection(settwo))
+print(f'identical elements in the sets are :', *setthree)
 
-s = {2,3,6,6,9}
-z = {1,1,7,6,9}
-print(type(z))
-
-r = int(input('enter length  '))
-f = {input (i) for i in range(r)} # генератор множеств
-print(f)
-print(s.symmetric_difference(z))
-print(s.difference(z))
-print(s.symmetric_difference(z))
-print(f"set ", s.intersection(z))  #111
-
-
-
-s1 = list(s)
-
-
-z1 = list(z)
-
-s1z1 = s1 + z1
-s2z2 = s1z1[:]
-
-print(s1z1)
-
-duplicates = []
-
-for item in s2z2:
-    if s2z2.count(item) > 1 and item not in duplicates:
-        duplicates.append(item)
-        print(sorted(duplicates))
-
-my_list = [1, 2, 3, 2, 4, 1, 5, 2]
-duplicates = []
-
-for item in my_list:
-    if my_list.count(item) > 1 and item not in duplicates:
-        duplicates.append(item)
-
-print("Повторяющиеся элементы в списке:", duplicates)
